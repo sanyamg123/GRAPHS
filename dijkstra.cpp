48bd0655563e972a489d805e7aca8825701617df
Sanyam Garg <sanyam@Codings-MacBook-Air.local>
2019-06-29 22:31:48 +0530

    graphs-1

---
+++ dijkstra.cpp
@@ -0,0 +1,136 @@
+#include<iostream>
+#include<stdio.h>
+#include<algorithm>
+#include<math.h>
+#include<bits/stdc++.h>
+#include<stack>
+#include<queue>
+#include<list>
+#include<vector>
+#include<bitset>
+#include<unordered_map> 
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+// #include "boost/algorithm/string.hpp"
+#define fio ios_base::sync_with_stdio(false)
+#define mod 1000000007
+#define mod1 mod
+#define mod2 1000000007
+#define li long long int
+#define ll li
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#define readi(x) scanf("%d",&x)
+#define  reads(x)  scanf("%s", x)
+#define readl(x) scanf("%I64d",&x)
+#define rep(i,n) for(i=0;i<n;i++)
+#define revp(i,n) for(i=(n-1);i>=0;i--)
+#define myrep1(i,a,b) for(i=a;i<=b;i++)
+#define myrep2(i,a,b) for(i=b;i>=a;i--)
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define sec second
+#define MAXN 1000000000000000005
+#define MINN -10000000000000000
+#define INTMAX 1000001000
+#define pii pair<ll,ll> 
+#define pdd pair<double,double>
+#define pic pair<int,char>
+#define N 1000005
+#define lgn 20
+#define ddouble long double
+#define minus minu
+#define PI 3.1415926535
+#define lgn 20
+ 
+ 
+// #define INTMAX 200000000 
+ 
+// using namespace boost;
+// #define si short int
+ 
+using namespace std;
+using namespace __gnu_pbds;             
+typedef priority_queue<ll, vector<ll > > max_pq;
+typedef priority_queue<pii, vector<pii> , greater<pii>  > min_pq;
+ll toint(const string &s) {stringstream ss; ss << s; ll x; ss >> x; return x;}
+string tostring ( ll number ){stringstream ss; ss<< number; return ss.str();}
+ 
+// using namespace __gnu_pbds;         
+// typedef priority_queue<pair<ll,char> , vector<pair<ll , char> > > max_pq;
+// typedef priority_queue<pii , vector<pii > ,greater<pii > > min_pq;
+typedef tree<pii , null_type, less<pii>, rb_tree_tag, tree_order_statistics_node_update> OST;
+// typedef priority_queue<pair < ll , pair < pii , ll > >  , vector<pair < ll , pair < pii , ll > >  > ,greater<pair < ll , pair < pii , ll > >  > > min_pq;
+#define TRACE
+#ifdef TRACE
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+    template <typename Arg1>
+    void __f(const char* name, Arg1&& arg1){
+        cout << name << " : " << arg1 << std::endl;
+        //use cerr if u want to display at the bottom
+    }
+    template <typename Arg1, typename... Args>
+    void __f(const char* names, Arg1&& arg1, Args&&... args){
+        const char* comma = strchr(names + 1, ','); cout.write(names, comma - names) << " : " << arg1<<" | ";__f(comma+1, args...);
+    }
+#else
+#define trace(...)
+#endif
+#define nn 2000005
+vector < vector < pii > > adjlist ( N );
+ll t,n,m,q;
+ll dist[ N ];
+bool visited[ N ];
+void dijkstra ( ll s )
+{
+    min_pq pq;
+    pq.push({0,s});
+    for ( ll i = 0; i <= n; i ++)
+        dist[i] = MAXN,visited[i] = 0;
+    ll ansmax = INT_MIN, cnt = 0;
+    while ( !pq.empty() )
+    {
+        ll d = pq.top().fi;
+        ll node = pq.top().sec;
+        pq.pop();
+        if ( dist[node] > d )
+        {
+            visited[node] = 1;
+            dist[node] = d;
+            if ( d > ansmax )
+                ansmax = d, cnt = 1;
+            else if ( d == ansmax )
+                cnt++;
+            for ( auto it : adjlist[node] )
+                if ( !visited[it.fi] )
+                    pq.push({d+it.sec,it.fi});
+        }
+    }
+    cout << ansmax <<" " << cnt <<"\n";
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    cin >> n >> m >> q;
+    for ( ll i = 1; i <= m; i ++)
+    {
+        ll a,b,c;
+        cin >> a >> b >> c;
+        adjlist[a].pb({b,c});
+        adjlist[b].pb({a,c});
+    }
+    while ( q-- )
+    {
+        ll s;
+        cin >> s;
+        dijkstra(s);
+    }
+
+}
