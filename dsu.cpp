fefa162f255b909d5196c2f8685db3c2a2107887
Sanyam Garg <sanyam@Codings-MacBook-Air.local>
2019-07-02 23:08:12 +0530

    graphs-2

---
+++ dsu.cpp
@@ -0,0 +1,131 @@
+#include<iostream>
+#include<stdio.h>
+#include<algorithm>
+#include<math.h>
+#include<bits/stdc++.h>
+#include<stack>
+#include<queue>
+#include<list>
+#include<vector>
+#include<bitset>
+#include<unordered_map> 
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+// #include "boost/algorithm/string.hpp"
+#define fio ios_base::sync_with_stdio(false)
+#define mod 1000000007
+#define mod1 mod
+#define mod2 1000000007
+#define li long long int
+#define ll li
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#define readi(x) scanf("%d",&x)
+#define  reads(x)  scanf("%s", x)
+#define readl(x) scanf("%I64d",&x)
+#define rep(i,n) for(i=0;i<n;i++)
+#define revp(i,n) for(i=(n-1);i>=0;i--)
+#define myrep1(i,a,b) for(i=a;i<=b;i++)
+#define myrep2(i,a,b) for(i=b;i>=a;i--)
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define sec second
+#define MAXN 1000000000000000005
+#define MINN -10000000000000000
+#define INTMAX 1000001000
+#define pii pair<ll,ll> 
+#define pdd pair<double,double>
+#define pic pair<int,char>
+#define N 1000005
+#define lgn 20
+#define ddouble long double
+#define minus minu
+#define PI 3.1415926535
+#define lgn 20
+ 
+ 
+// #define INTMAX 200000000 
+ 
+// using namespace boost;
+// #define si short int
+ 
+using namespace std;
+using namespace __gnu_pbds;             
+typedef priority_queue<ll, vector<ll > > max_pq;
+typedef priority_queue<pii, vector<pii> , greater<pii>  > min_pq;
+ll toint(const string &s) {stringstream ss; ss << s; ll x; ss >> x; return x;}
+string tostring ( ll number ){stringstream ss; ss<< number; return ss.str();}
+ 
+// using namespace __gnu_pbds;         
+// typedef priority_queue<pair<ll,char> , vector<pair<ll , char> > > max_pq;
+// typedef priority_queue<pii , vector<pii > ,greater<pii > > min_pq;
+typedef tree<pii , null_type, less<pii>, rb_tree_tag, tree_order_statistics_node_update> OST;
+// typedef priority_queue<pair < ll , pair < pii , ll > >  , vector<pair < ll , pair < pii , ll > >  > ,greater<pair < ll , pair < pii , ll > >  > > min_pq;
+#define TRACE
+#ifdef TRACE
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+    template <typename Arg1>
+    void __f(const char* name, Arg1&& arg1){
+        cout << name << " : " << arg1 << std::endl;
+        //use cerr if u want to display at the bottom
+    }
+    template <typename Arg1, typename... Args>
+    void __f(const char* names, Arg1&& arg1, Args&&... args){
+        const char* comma = strchr(names + 1, ','); cout.write(names, comma - names) << " : " << arg1<<" | ";__f(comma+1, args...);
+    }
+#else
+#define trace(...)
+#endif
+#define nn 2000005
+multiset < ll > s;
+ll n,q;
+ll par[ N ],ran[ N ],sz[ N ];
+int find_set(int v) {
+    if (v == par[v])
+        return v;
+    return par[v] = find_set(par[v]);
+}
+void make_set(int v) {
+    par[v] = v;
+    ran[v] = 0;
+    sz[v] = 1;
+    s.insert(1);
+}
+void union_sets(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b) {
+        if (ran[a] < ran[b])
+            swap(a, b);
+        par[b] = a;
+        s.erase(s.find(sz[a]));
+        s.erase(s.find(sz[b]));
+        sz[a] += sz[b];
+        s.insert(sz[a]);
+        if (ran[a] == ran[b])
+            ran[a]++;
+    }
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    cin >> n >> q;
+    for ( ll i = 1; i <= n; i ++)
+        make_set(i);
+    while ( q-- )
+    {
+        ll a,b;
+        cin >> a >> b;
+        union_sets(a,b);
+        multiset < ll > :: iterator it = (--s.end());
+        cout <<(*it)-(*s.begin()) <<"\n";
+    }
+
+}
---
+++ n^4dp.cpp
@@ -0,0 +1,123 @@
+#include<iostream>
+#include<stdio.h>
+#include<algorithm>
+#include<math.h>
+#include<bits/stdc++.h>
+#include<stack>
+#include<queue>
+#include<list>
+#include<vector>
+#include<bitset>
+#include<unordered_map> 
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+// #include "boost/algorithm/string.hpp"
+#define fio ios_base::sync_with_stdio(false)
+#define mod 1000000007
+#define mod1 mod
+#define mod2 1000000007
+#define li long long int
+#define ll li
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#define readi(x) scanf("%d",&x)
+#define  reads(x)  scanf("%s", x)
+#define readl(x) scanf("%I64d",&x)
+#define rep(i,n) for(i=0;i<n;i++)
+#define revp(i,n) for(i=(n-1);i>=0;i--)
+#define myrep1(i,a,b) for(i=a;i<=b;i++)
+#define myrep2(i,a,b) for(i=b;i>=a;i--)
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define sec second
+#define MAXN 1000000000000000005
+#define MINN -10000000000000000
+#define INTMAX 1000001000
+#define pii pair<ll,ll> 
+#define pdd pair<double,double>
+#define pic pair<int,char>
+#define N 1000005
+#define lgn 20
+#define ddouble long double
+#define minus minu
+#define PI 3.1415926535
+#define lgn 20
+ 
+ 
+// #define INTMAX 200000000 
+ 
+// using namespace boost;
+// #define si short int
+ 
+using namespace std;
+using namespace __gnu_pbds;             
+typedef priority_queue<ll, vector<ll > > max_pq;
+typedef priority_queue<pii, vector<pii> , greater<pii>  > min_pq;
+ll toint(const string &s) {stringstream ss; ss << s; ll x; ss >> x; return x;}
+string tostring ( ll number ){stringstream ss; ss<< number; return ss.str();}
+ 
+// using namespace __gnu_pbds;         
+// typedef priority_queue<pair<ll,char> , vector<pair<ll , char> > > max_pq;
+// typedef priority_queue<pii , vector<pii > ,greater<pii > > min_pq;
+typedef tree<pii , null_type, less<pii>, rb_tree_tag, tree_order_statistics_node_update> OST;
+// typedef priority_queue<pair < ll , pair < pii , ll > >  , vector<pair < ll , pair < pii , ll > >  > ,greater<pair < ll , pair < pii , ll > >  > > min_pq;
+#define TRACE
+#ifdef TRACE
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+    template <typename Arg1>
+    void __f(const char* name, Arg1&& arg1){
+        cout << name << " : " << arg1 << std::endl;
+        //use cerr if u want to display at the bottom
+    }
+    template <typename Arg1, typename... Args>
+    void __f(const char* names, Arg1&& arg1, Args&&... args){
+        const char* comma = strchr(names + 1, ','); cout.write(names, comma - names) << " : " << arg1<<" | ";__f(comma+1, args...);
+    }
+#else
+#define trace(...)
+#endif
+#define nn 2000005
+ll dist[ 200 ][ 200 ];
+ll n,m;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    cin >> n >> m;
+    for ( ll i = 1; i <= n;i ++)
+    {
+        for ( ll j = 1; j <= n; j ++)
+        {
+            dist[i][j] = INT_MAX;
+            w[i][j] = INT_MAX;
+        }
+        w[i][i] = 0;
+    }
+    for ( ll i = 1; i <= m; i ++)
+    {
+        ll a,b,c;
+        cin >> a >> b >> c;
+        dist[a][b] = c;
+        w[a][b] = c;
+    }
+    for ( ll x = 1; x <= n-1; x ++)
+    {
+        for ( ll k = 1; k <= n; k ++)
+        {
+            for ( ll i = 1; i <= n; i ++)
+            {
+                for ( ll j = 1; j <= n; j++)
+                {
+                    dist[i][j] = min(dist[i][j],dist[i][k]+w[k][j]);
+                }
+            }
+        }   
+    }
+    
+}
